65c785e29248855445bd3ca40da2b4a4042df604
YiweiZhang-lab <55486104+YiweiZhang-lab@users.noreply.github.com>
2025-06-18 19:16:09 +0800

    Update vali_KARDust_20211231.m

--- vali_KARDust_20211231.m
+++ vali_KARDust_20211231.m
@@ -4,98 +4,89 @@
 % % Date     : Mar-2025, Sep-2024
 % % Note     : 
 
-close all
-clear
-clc;
+close all; clear; clc;
 
 %% 本论文方法
 % % 加载r
-load('data\KARDust_20211231\Rhor_385H.mat');
-load('data\KARDust_20211231\Rhor_865.mat');
+load('KARDust_20211231\Rhor_385H_clip.mat');
+load('KARDust_20211231\Rhor_865_clip.mat');
       
 % % 加载RC
-load('data\KARDust_20211231\Rhorc_385H.mat');
-load('data\KARDust_20211231\Rhor_412.mat');
-load('data\KARDust_20211231\Rhor_750.mat');
-load('data\KARDust_20211231\Rhorc_865.mat');
-
+load('KARDust_20211231\Rhorc_385H_clip.mat');
+load('KARDust_20211231\Rhorc_412_clip.mat');
+load('KARDust_20211231\Rhorc_750_clip.mat');
+load('KARDust_20211231\Rhorc_865_clip.mat');
 
 % % 加载角度
-load('data\KARDust_20211231\B3_Satellite_Zenith_Angle_rec.mat');
-load('data\KARDust_20211231\B3_Sun_Zenith_Angle_rec.mat')
-load('data\KARDust_20211231\B3_RAA_rec.mat')
+load('KARDust_20211231\B3_Satellite_Zenith_Angle_clip.mat');
+load('KARDust_20211231\B3_Sun_Zenith_Angle_clip.mat');
+load('KARDust_20211231\B3_RAA_clip.mat');
 
-load('data\KARDust_20211231\Satellite_Zenith_Angle_rec.mat');
-load('data\KARDust_20211231\Sun_Zenith_Angle_rec.mat')
-load('data\KARDust_20211231\RAA_rec.mat')
+load('KARDust_20211231\Satellite_Zenith_Angle_clip.mat');
+load('KARDust_20211231\Sun_Zenith_Angle_clip.mat');
+load('KARDust_20211231\RAA_clip.mat');
 
 % % 加载经纬度
-load('data\KARDust_20211231\Lon.mat');
-load('data\KARDust_20211231\Lat.mat');
+load('KARDust_20211231\Lon_clip.mat');
+load('KARDust_20211231\Lat_clip.mat');
 
-VZA_UVI = B3_Satellite_Zenith_Angle_rec; VZA_UVI(VZA_UVI<0) = nan; 
-SZA_UVI = B3_Sun_Zenith_Angle_rec; SZA_UVI(VZA_UVI<0) = nan;
-RAA_UVI = B3_RAA_rec;
+VZA_UVI_clip = B3_Satellite_Zenith_Angle_clip; VZA_UVI_clip(VZA_UVI_clip<-900) = nan; 
+SZA_UVI_clip = B3_Sun_Zenith_Angle_clip; SZA_UVI_clip(VZA_UVI_clip<-900) = nan;
+RAA_UVI_clip = B3_RAA_clip; RAA_UVI_clip(RAA_UVI_clip<-900) = nan;
 
-VZA_OCT = Satellite_Zenith_Angle_rec;  VZA_OCT(VZA_OCT<0) = nan;
-SZA_OCT = Sun_Zenith_Angle_rec;  SZA_OCT(SZA_OCT<0) = nan;
-RAA_OCT = RAA_rec;
+VZA_OCT_clip = Satellite_Zenith_Angle_clip;  VZA_OCT_clip(VZA_OCT_clip<-900) = nan;
+SZA_OCT_clip = Sun_Zenith_Angle_clip;  SZA_OCT_clip(SZA_OCT_clip<-900) = nan;
+RAA_OCT_clip = RAA_clip; RAA_OCT_clip(RAA_OCT_clip<-900) = nan;
 
-xmus_UVI = cosd(SZA_UVI);
-xmuv_UVI = cosd(VZA_UVI);
-xmup_UVI = cosd(RAA_UVI);
-xmud_UVI = -xmus_UVI.*xmuv_UVI-sqrt(1.-xmus_UVI.*xmus_UVI).*sqrt(1.-xmuv_UVI.*xmuv_UVI).*xmup_UVI;
-SCA_UVI = acosd(xmud_UVI); 
+xmus_UVI_clip = cosd(SZA_UVI_clip);
+xmuv_UVI_clip = cosd(VZA_UVI_clip);
+xmup_UVI_clip = cosd(RAA_UVI_clip);
+xmud_UVI_clip = -xmus_UVI_clip.*xmuv_UVI_clip-sqrt(1.-xmus_UVI_clip.*xmus_UVI_clip).*sqrt(1.-xmuv_UVI_clip.*xmuv_UVI_clip).*xmup_UVI_clip;
+SCA_UVI_clip = acosd(xmud_UVI_clip); 
 
-xmus_OCT = cosd(SZA_OCT);
-xmuv_OCT = cosd(VZA_OCT);
-xmup_OCT = cosd(RAA_OCT);
-xmud_OCT = -xmus_OCT.*xmuv_OCT-sqrt(1.-xmus_OCT.*xmus_OCT).*sqrt(1.-xmuv_OCT.*xmuv_OCT).*xmup_OCT;
-SCA_OCT = acosd(xmud_OCT); 
+xmus_OCT_clip = cosd(SZA_OCT_clip);
+xmuv_OCT_clip = cosd(VZA_OCT_clip);
+xmup_OCT_clip = cosd(RAA_OCT_clip);
+xmud_OCT_clip = -xmus_OCT_clip.*xmuv_OCT_clip-sqrt(1.-xmus_OCT_clip.*xmus_OCT_clip).*sqrt(1.-xmuv_OCT_clip.*xmuv_OCT_clip).*xmup_OCT_clip;
+SCA_OCT_clip = acosd(xmud_OCT_clip); 
 
 %%
-Rhorc_i_385H(Rhorc_i_385H<0)=nan;  
-Rhorc_i_750(Rhorc_i_750<0)=nan;
-Rhorc_i_865(Rhorc_i_865<0)=nan;
+Rhorc_i_385H_clip(Rhorc_i_385H_clip<0)=nan;
+Rhorc_i_412_clip(Rhorc_i_412_clip<0)=nan;  
+Rhorc_i_750_clip(Rhorc_i_750_clip<0)=nan;
+Rhorc_i_865_clip(Rhorc_i_865_clip<0)=nan;
 
 %% 
-A = log10(Rhorc_i_385H);
-B = log10(Rhorc_i_750);
-C = Rhor_i_385H_rec - Rhor_i_865_rec;
-D = 1+cosd(VZA_OCT);
-E = 1+sind(SCA_OCT);
-UVDAI = (A - B)./(C .* D .* E);
+AA = log10(Rhorc_i_385H_clip./Rhorc_i_865_clip);
+BB = log10(Rhorc_i_750_clip./Rhorc_i_865_clip);
+CC = Rhor_i_385H_cor_clip - Rhor_i_865_cor_clip;
+DD = 1+cosd(VZA_OCT_clip);
+EE = 1+sind(SCA_OCT_clip);
+UVDAI_clip = (AA - BB)./(CC .* DD .* EE);
 
 % % 
-indCld = find((Rhorc_i_865 + Rhorc_i_750)>0.263);
+indCld = find((Rhorc_i_865_clip + Rhorc_i_750_clip)>0.312 | Rhorc_i_385H_clip>0.155);
 
-UVDAI(isnan(UVDAI)) = 999;
-UVDAI_Cld = UVDAI;
-UVDAI_Cld(indCld) = nan;
+UVDAI_clip(isnan(UVDAI_clip)) = 999;
+UVDAI_clip_Cld = UVDAI_clip;
+UVDAI_clip_Cld(indCld) = nan;
 
 %% ********************* 本论文方法 ************************
 % % UVDAI 分布展示
 load('mycmap1.mat');
 figure();
-% set(gcf,'unit','centimeters','position', [15 5 6.17 6.17]);
-% set(gca,'position', [0.05 0.05 0.9 0.9])
-m_proj('miller cylindrical','lat',[34.00 43.00],'lon',[127.00 137.00]);
-
-m_pcolor(Lon, Lat, UVDAI_Cld);
+axis normal;
+m_proj('miller cylindrical','lat',[19.995 27.005],'lon',[61.995 70.005]);
+m_pcolor(Lon_clip, Lat_clip, UVDAI_clip_Cld);
 shading interp;
 hold on;
 colormap(mycmap1);
 caxis([-2.5 2.5]);
-% cb = colorbar('horiz');
-% set(cb, 'FontSize', 12, 'FontWeight', 'bold', 'FontName', 'Times New Roman');
-% cb.TickLabels ={'-2.5','-2.0','-1.5','-1.0','-0.5','0','0.5','1.0'};
+m_gshhs('lc1', 'patch', [0.5 0.5 0.5], 'edgecolor', 'k');
+m_grid('XTick', 63:3:69, 'XTickLabels', [], 'YTick', 21:3:27, 'YTickLabels', [], ...
+       'tickdir', 'out', 'xlabeldir', 'middle', 'fontsize', 10, 'backcolor',[1 1 1], 'box', 'on');
 
-axis equal; % 使xy轴等比例
-m_gshhs('lc1', 'patch',[0.5 0.5 0.5],'edgecolor','k');
-m_grid('XTick', 127:3:136, 'XTickLabels', [], 'YTick', 34:3:43, 'YTickLabels', [], ...
-       'tickdir', 'out', 'xlabeldir', 'middle','fontsize',10, 'backcolor',[1 1 1], 'box', 'on');
-
-print('-dpng','-r1000',['UVDAI_KARDust_20211231','.png'])
+print('-dpng', '-r900', ['UVDAI_KARDust_20211231_1', '.png'])
 
 % ******************** 分类结果展示 *********************************
 % % 阈值为 r，UVDAI < r 则为沙尘气溶胶； UVDAI > r 则为清洁大气
@@ -116,48 +107,43 @@ mycmap1(len1:end, 3) = 0.07;
 
 
 figure(); clf;
-% % 设置图像大小
-% set(gcf,'unit','centimeters','position', [17 7 6.17 6.17]);
-% set(gca,'position',[0.05 0.05 0.9 0.9])
-
-m_proj('miller cylindrical','lat',[34.00 43.00],'lon',[127.00 137.00]);
-m_pcolor(Lon, Lat, UVDAI_Cld);
-shading interp;
+axis normal;
+m_proj('miller cylindrical','lat',[19.995 27.005],'lon',[61.995 70.005]);
 hold on;
+m_pcolor(Lon_clip, Lat_clip, UVDAI_clip_Cld);
+shading interp;
 colormap(mycmap1);
 caxis([-2.5 2.5]);
-
-axis equal; % 使xy轴等比例
-
-m_gshhs('lc1', 'patch',[0.5 0.5 0.5],'edgecolor','k');
-m_grid('XTick', 127:3:136, 'XTickLabels', [], 'YTick', 34:3:43, 'YTickLabels', [], ...
+m_gshhs('lc1', 'patch', [0.5 0.5 0.5], 'edgecolor', 'k');
+m_grid('XTick', 63:3:69, 'XTickLabels', [], 'YTick', 21:3:27, 'YTickLabels', [], ...
        'tickdir', 'out', 'xlabeldir', 'middle','fontsize',10, 'backcolor',[1 1 1], 'box', 'on');
 
-print('-dpng','-r1000',['Class_UVDAI_KARDust_20211231','.png'])
+print('-dpng','-r600',['Class_UVDAI_KARDust_20211231_1','.png'])
 
 %% ********************* ShiWang07 论文方法 ***************************
-Ab = Rhorc_i_412 ./ Rhorc_i_865 .* exp(-453/115 * log(Rhorc_i_750 ./ Rhorc_i_865));
+Ab_clip = Rhorc_i_412_clip ./ Rhorc_i_865_clip .* exp(-453/115 * log(Rhorc_i_750_clip ./ Rhorc_i_865_clip));
 
-Ab(isnan(Ab)) = 999;
-Ab_Cld = Ab;
-Ab_Cld(indCld) = nan;
+Ab_clip(isnan(Ab_clip)) = 999;
+Ab_clip_Cld = Ab_clip;
+Ab_clip_Cld(indCld) = nan;
 
-load('mycmap1.mat');
-% % % % % % 展示 Absind 分布结果 --------------------
+load('mycmap2.mat');
 figure(); clf;
-m_proj('miller cylindrical','lat',[34.00 43.00],'lon',[127.00 137.00]);
-m_pcolor(Lon, Lat, Ab_Cld);
+% % 设置图像大小
+% set(gcf,'unit','centimeters','position', [7 5 6.17 7.69]);
+axis normal;
+% set(gca,'position',[0.05 0.05 0.9 0.9])
+
+m_proj('miller cylindrical','lat',[19.995 27.005],'lon',[61.995 70.005]);
+m_pcolor(Lon_clip, Lat_clip, Ab_clip_Cld);
 shading interp;
-hold on;
-colormap(mycmap1);
-caxis([0 2]);  
-axis equal;
-
+colormap(mycmap2);
+caxis([0 2]);
 m_gshhs('lc1', 'patch',[0.5 0.5 0.5],'edgecolor','k');
-m_grid('XTick', 127:3:136, 'XTickLabels', [], 'YTick', 34:3:43, 'YTickLabels', [], ...
+m_grid('XTick', 63:3:69, 'XTickLabels', [], 'YTick', 21:3:27, 'YTickLabels', [], ...
        'tickdir', 'out', 'xlabeldir', 'middle','fontsize',10, 'backcolor',[1 1 1], 'box', 'on');
-
-print('-dpng','-r1000',['AAI_KARDust_20211231','.png']);
+       
+print('-dpng','-r600',['AAI_KARDust_20211231_1','.png']);
 
 % % % % % % % % % % % % % % % 分类结果展示 % % % % % % % % % % % % %
 % % 阈值为 r2，AAI < r2 则为沙尘气溶胶； AAI > r2 则为清洁大气
@@ -177,17 +163,14 @@ mycmap2(len1:end, 2) = 0.99;
 mycmap2(len1:end, 3) = 0.07;
 
 figure(); clf;
-
-m_proj('miller cylindrical','lat',[34.00 43.00],'lon',[127.00 137.00]);
-m_pcolor(Lon, Lat, Ab_Cld);
+axis normal;
+m_proj('miller cylindrical','lat',[19.995 27.005],'lon',[61.995 70.005]);
+m_pcolor(Lon_clip, Lat_clip, Ab_clip);
 shading interp;
 colormap(mycmap2);
 caxis([minL maxL]);
-
-axis equal;
-
-m_gshhs('lc1', 'patch',[0.5 0.5 0.5],'edgecolor','k');
-m_grid('XTick', 127:3:136, 'XTickLabels', [], 'YTick', 34:3:43, 'YTickLabels', [], ...
+m_gshhs('lc1', 'patch', [0.5 0.5 0.5], 'edgecolor', 'k');
+m_grid('XTick', 63:3:69, 'XTickLabels', [], 'YTick', 21:3:27, 'YTickLabels', [], ...
        'tickdir', 'out', 'xlabeldir', 'middle','fontsize',10, 'backcolor',[1 1 1], 'box', 'on');
 
-print('-dpng','-r1000',['Class_AAI_KARDust_20211231','.png'])
+print('-dpng','-r600',['Class_AAI_KARDust_20211231_1','.png'])
